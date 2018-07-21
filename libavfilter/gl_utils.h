e7559c5b0b02d6d0212ba4fa0eeb2bcab703750a
Mengbai Xiao <xiaomengbai@gmail.com>
2018-07-20 20:57:46 -0400

    remove warnings

--- libavfilter/gl_utils.h
+++ libavfilter/gl_utils.h
@@ -14,6 +14,12 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#ifndef GLFW_TRUE
+#define GLFW_TRUE GL_TRUE
+#endif
+#ifndef GLFW_FALSE
+#define GLFW_FALSE GL_FALSE
+#endif
 
 static const double PI = 3.14159265358979323846;
 
@@ -49,73 +55,5 @@ int CheckGLError(void *avctx, const char *error_message);
 GLuint LoadShader(void *avctx, const char* filename, GLenum shader_type);
 
 
-
-#define ITEM_STR_LEN 128
-#define ENLARGE_ITEM_NR 20
-
-typedef union {
-    char str[ITEM_STR_LEN];
-
-    uint64_t u64;
-
-    uint32_t u32;
-
-    int64_t  i64;
-
-    int32_t  i32;
-
-    void *ptr;
-
-} vector_item_t;
-
-typedef struct {
-    vector_item_t *head;
-
-    uint64_t nr;
-
-    uint64_t size;
-} vector_t;
-
-static vector_t *init_vector(void)
-{
-    vector_t *v = (vector_t *)malloc(sizeof(vector_t));
-    v->head = NULL;
-    v->nr = 0;
-    v->size = 0;
-    return v;
-}
-
-static void destroy_vector(vector_t *v)
-{
-    if(v->size)
-        free(v->head);
-    free(v);
-}
-
-static void *enlarge_vector(vector_t *v)
-{
-    v->size += ENLARGE_ITEM_NR;
-    v->head = realloc(v->head, sizeof(vector_item_t) * v->size);
-    return v;
-}
-
-static void push_back(vector_t *v, vector_item_t item)
-{
-    if(v->nr >= v->size)
-        enlarge_vector(v);
-    memcpy(&v->head[v->nr], &item, sizeof(vector_item_t));
-    /* v->head[v->nr] = item; */
-    v->nr++;
-}
-
-/* static void pr_items_str(vector_t *v) */
-/* { */
-/*     int i; */
-/*     printf("items (%lu/%lu):\n", v->nr, v->size); */
-/*     for(i = 0; i < v->nr; i++){ */
-/*         printf("  %s\n", v->head[i].str); */
-/*     } */
-/*     printf("\n"); */
-/* } */
 
 #endif
